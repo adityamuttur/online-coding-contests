5aafac6c62e7cd0aba69ede8c0e20d3a5acd44f8
sourcecode123 <amuttur123@gmail.com>
2013-10-20 14:03:24 +0530

    HackerRank question added

---
+++ HackerRank/October2020/ChocolateFeast.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	int T, N, C, M;
+	int sweets, wrappers, new_sweets;
+
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++) {
+		scanf("%d %d %d", &N, &C, &M);
+		sweets = wrappers = N/C;
+		while (wrappers >= M) {
+			new_sweets = wrappers/M;
+			wrappers = (wrappers%M) + new_sweets;
+			sweets += new_sweets;
+		}
+		printf("%d\n", sweets);
+	}
+
+	return 0;
+}
+
